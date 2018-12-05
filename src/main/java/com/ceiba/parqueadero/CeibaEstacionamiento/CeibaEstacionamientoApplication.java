504d6c568e586caf3a904571f5ebcde6ff573ca4
Maria Ocampo <maria.ocampo@ceiba.com.co>
2018-12-05 08:17:16 -0500

    Se crea estructura incial del proyecto

---
+++ src/main/java/com/ceiba/parqueadero/CeibaEstacionamiento/CeibaEstacionamientoApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.parqueadero.CeibaEstacionamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaEstacionamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaEstacionamientoApplication.class, args);
+	}
+}
