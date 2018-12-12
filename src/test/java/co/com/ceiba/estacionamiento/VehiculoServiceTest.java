9353a0a535795e4f814cf95681da588555671c4a
Maria Ocampo <maria.ocampo@ceiba.com.co>
2018-12-12 10:32:54 -0500

    Nuevos test

---
+++ src/test/java/co/com/ceiba/estacionamiento/VehiculoServiceTest.java
@@ -0,0 +1,37 @@
+package co.com.ceiba.estacionamiento;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import co.com.ceiba.estacionamiento.entity.Vehiculo;
+import co.com.ceiba.estacionamiento.repository.VehiculoRepository;
+import co.com.ceiba.estacionamiento.service.impl.VehiculoServiceImpl;
+import co.com.ceiba.estacionamiento.utils.MapeoDTO;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VehiculoServiceTest {
+
+	@InjectMocks
+	VehiculoServiceImpl vehiculoService = new VehiculoServiceImpl();
+	
+	@Mock
+	VehiculoRepository vehiculoRepository;
+	
+	MapeoDTO mapeoDTO = new MapeoDTO();
+	
+	@Test
+	public void debeGuardarUnNuevoVehiculo() {
+		//Arrange
+		when(vehiculoRepository.save(Mockito.any(Vehiculo.class))).thenReturn(new Vehiculo());
+		
+		//act
+		vehiculoService.registrarVehiculo(new Vehiculo());
+
+	}
+}
